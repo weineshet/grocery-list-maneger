1e88d7a0a863e8dcd6aabb7cdfbc1f53da28f93a
weineshet <52981354+weineshet@users.noreply.github.com>
2019-09-21 21:21:02 -0700

    changes

---
+++ src/app/app.grocery.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-grocery",
+  templateUrl: "./app.grocery.html",
+  styleUrls: ["../assets/css/bootstrap.min.css"]
+})
+export class GroceryComponent {
+  task: string;
+  tasks = [];
+  onClick() {
+    //console.log(this.task);
+    this.tasks.push({ name: this.task });
+    this.task = "";
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroceryComponent } from './app.grocery';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [GroceryComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GroceryComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
